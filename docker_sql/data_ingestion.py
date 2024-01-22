2ead3aa19fe58b79086a65a263b64c12530c2f6f
Filip Livancic <filiplivancic@gmail.com>
2024-01-22 15:54:50 +0000

    added container update shell script

--- docker_sql/data_ingestion.py
+++ docker_sql/data_ingestion.py
@@ -3,7 +3,8 @@ import sys
 import argparse
 import pandas as pd
 import psycopg2
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.exc import NoSuchTableError
 
 def main(params):
     user = params.user
@@ -14,12 +15,25 @@ def main(params):
 
     engine=create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     engine.connect()
+    meta_data = MetaData()
+    
 
     folder_split = sys.path[0].split('\\')[0:-1]
     base_folder = '\\'.join(folder_split)
     data_folder = os.path.join(base_folder,'data')
 
+    # Dropping Old Tables
+    for item in ['games','players','season_averages','teams']:
+        try:
+            table = Table(item, meta_data, autoload_with=engine)
+            table.drop(engine)
+            print(f'Dropped: {item}')
+        except NoSuchTableError:
+            print(f'{item} - Table Does Not Exist')
+
+    # Ingesting New Tables From Scratch
     for filename in os.listdir(data_folder):
+        print(f'Ingesting: {filename}')
         table_name = filename.split('.')[0]
         df = pd.read_json(os.path.join(data_folder,filename))
         if filename.split('.')[0] == 'players':
--- docker_sql/py_ingest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run Docker Image Containing Python Ingestion Script
-winpty docker start -i pyingest
